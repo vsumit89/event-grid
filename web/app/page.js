aac9b34a6871d2a1cf7a2980b06598fe9896a1e6
vsumit89 <vsumit030201@gmail.com>
2024-05-26 09:06:55 +0530

    feat: added date formatter component

--- web/app/page.js
+++ web/app/page.js
@@ -7,23 +7,34 @@ import { CalendarGrid } from '@/components/calendar/calendarGrid'
 import { CalendarSettings } from '@/components/calendar/settings'
 import { Input } from '@/components/form/input'
 import { Backdrop } from '@/components/modal/backdrop'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
     const [selectedDate, setSelectedDate] = useState(new Date())
+
+    const [numDays, setNumDays] = useState(6)
 
     const [selectedDuration, setSelectedDuration] = useState(15)
 
     const [dateRange, setDateRange] = useState({
         initialDate: selectedDate,
-        finalDate: addDaysToDate(selectedDate, 6),
+        finalDate: addDaysToDate(selectedDate, numDays),
     })
 
     const [showCreateForm, setShowCreateForm] = useState(false)
 
+    const [timeFormat, setTimeFormat] = useState(24)
+
     const handleCreateFormVisibility = () => {
         setShowCreateForm(!showCreateForm)
     }
+
+    useEffect(() => {
+        setDateRange({
+            initialDate: selectedDate,
+            finalDate: addDaysToDate(selectedDate, numDays),
+        })
+    }, [numDays])
 
     return (
         <main className="flex w-screen h-screen bg-primary-background relative">
@@ -61,24 +72,36 @@ export default function Home() {
                     setSelectedDate={setSelectedDate}
                     dateRange={dateRange}
                     setDateRange={setDateRange}
+                    numDays={numDays}
+                    setNumDays={setNumDays}
+                    timeFormat={timeFormat}
+                    setTimeFormat={setTimeFormat}
                 />
                 <div className="flex flex-1 border-t border-primary-border">
                     <CalendarGrid
                         startDate={dateRange.initialDate}
                         endDate={dateRange.finalDate}
                         toggleForm={handleCreateFormVisibility}
+                        timeFormat={timeFormat}
+                        openDate={(date) => {
+                            setSelectedDate(date)
+                            setDateRange({
+                                initialDate: date,
+                                finalDate: addDaysToDate(date, 0),
+                            })
+
+                            setNumDays(0)
+                        }}
                     />
                 </div>
             </div>
             {showCreateForm ? (
-                <Backdrop 
-                  onClose={() => setShowCreateForm(false)}
-                >
-                    <div 
-                      className="p-6 bg-white text-black w-2/5 rounded-md"
-                      onClick={(e) => {
-                        e.stopPropagation()
-                      }}
+                <Backdrop onClose={() => setShowCreateForm(false)}>
+                    <div
+                        className="p-6 bg-white text-black w-2/5 rounded-md"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                        }}
                     >
                         <div className="">Sumit</div>
                     </div>
